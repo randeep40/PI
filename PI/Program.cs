53b4ce5cf9713f37448847b82ad8b01bfe542bd1
randeep40 <randeep40@users.noreply.github.com>
2019-07-18 16:31:18 +0530

    Create Program.cs

---
+++ PI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PI
+{
+
+    // C# program to demonstrate the 
+    // Constant values of Math.E function 
+    using System;
+
+    class GFG
+    {
+
+        // Main method 
+        static void Main()
+        {
+
+            // To find E constant values 
+            double e = Math.E;
+
+            // Print result 
+            Console.WriteLine("Math.E  = " + e);
+        }
+    } 
+}
